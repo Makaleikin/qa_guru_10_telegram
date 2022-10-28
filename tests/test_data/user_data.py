fc0a551dca6e19abb4db28aa41fbf694cfca9c3a
Artem <armlynskii@gmail.com>
2022-10-28 12:56:03 +0700

    added allure-notifications report to telegram

--- tests/test_data/user_data.py
+++ tests/test_data/user_data.py
@@ -36,7 +36,7 @@ class User:
     current_address: str = 'The Earth'
     picture_file: str = './tests/test_data/qapicture.png'
     state: str = 'NCR'
-    city: str = 'Gurgaon'
+    city: str = 'Delhi'
 
 
 student = User(first_name='Artem', gender=Gender.Male)
