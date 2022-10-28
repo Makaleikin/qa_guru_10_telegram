85c3b922df1743560cd04e38bcc0480d66dbf500
Artem <armlynskii@gmail.com>
2022-10-28 12:54:48 +0700

    added allure-notifications report to telegram

--- demoqa_tests/model/contols/dropdown.py
+++ demoqa_tests/model/contols/dropdown.py
@@ -5,5 +5,5 @@ from selene.support.shared import browser
 def select(element, option):
     element.click()
     browser.all('[id^=react-select][id*=-option-]').by(
-        have.exact_text(option)
-    ).press_enter()
+        have.text(option)
+    ).first.click()
